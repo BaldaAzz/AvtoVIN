1ad5d101aff3b9a60e5e08c35d4db9b2ba885c31
BaldaAzz <kill200419@gmail.com>
2024-11-11 23:46:56 +0300

    Создал проект

---
+++ src/main/java/com/home/AvtoVIN/AvtoVinApplication.java
@@ -0,0 +1,13 @@
+package com.home.AvtoVIN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvtoVinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvtoVinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/AvtoVIN/AvtoVinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.AvtoVIN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvtoVinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
